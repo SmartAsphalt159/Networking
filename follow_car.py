1792ecd9d8d375c6779fe5445d50c816dafe7da2
akettle44 <andrewjkettle@gmail.com>
2020-11-09 19:47:45 -0800

    Successfully broadcasting, not receiving yet

--- follow_car.py
+++ follow_car.py
@@ -3,22 +3,42 @@
 #import socket module
 import socket
 from Packet import Packet
-import _thread
+import time
+import threading
 
-#create new socket and get host name
-soc = socket.socket() #new socket
-hname = socket.gethostname()
+def main():
 
-#need two sockets for p2p, configed in opposite manner
-listen_port = 6201
-send_port = 6200
+    ######## BEGIN SOCKET INITIALIZATION ########
+    sndskt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      #using ipv4 address + UDP packets
+    recskt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      #using ipv4 address + UDP packets
+    sndskt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)   #broadcasting for now but want to explore multicasting later on
 
-#bind socket to local host on the listening side
-soc.bind((hname, listen_port))
+    #need two sockets for p2p, configed in opposite manner
+    listen_port = 6200
+    send_port = 6201
 
-soc.connect((hname, send_port))
+    recskt.bind(("192.168.1.255", listen_port))                    #bind socket to local IP for listening
+    sndskt.connect(("192.168.1.255", send_port))                   #connecting on the braodcast port
+    ######## END SOCKET INITIALIZATION ########
 
-recpkt = Packet(0, 0, 0)
+    #spawn thread for listening
+    list_thread = threading.Thread(target=listening, args=([recskt]))
+    list_thread.start()
 
-recpkt.decode_pkt(soc.recv(1024))
-print(recpkt.identifier, recpkt.steering, recpkt.speed)
+    bcast(sndskt)
+
+def bcast(sskt):
+    while(1): #replace with flag for getting new sample later on
+        sskt.send(("Car 1 packet is broadcasting").encode())     #replace with packet encoding
+        time.sleep(2)
+
+def listening(rskt):
+    print(rskt.getsockname())
+    while(1):
+        data, address = rskt.recvfrom(2048)
+        print(address)
+        if(address == "192.168.1.35"):
+            print(data)
+
+if __name__ == "__main__":
+    main()
